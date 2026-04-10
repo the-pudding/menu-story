1b1b8bd4b94c34837ae45c41f65e5068df22b6c1
Matt Daniels <matt@polygraph.cool>
2026-04-10 16:29:38 -0400

    update github pages

---
+++ docs/_app/immutable/entry/start.DBq4Y6gn.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DVp319iQ.js";import{x as t}from"../chunks/DaTg3iXA.js";export{t as load_css,r as start};
--- docs/_app/immutable/entry/start.Nsex2cEu.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BmDHEwZd.js";import{x as t}from"../chunks/CHuj2_a9.js";export{t as load_css,r as start};
