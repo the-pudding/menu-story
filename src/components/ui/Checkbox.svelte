efe69c439e61986b0b2cc7fd8209006161810486
matthewfdaniels <matthew.f.daniels@gmail.com>
2026-04-10 12:38:55 -0400

    Initial commit

---
+++ src/components/ui/Checkbox.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Checkbox, Label, useId } from "bits-ui";
+	let {
+		id = useId(),
+		checked = $bindable(false),
+		size = "default", // "default" | "sm"
+		labelText,
+		labelPosition = "right", // "right" | "left" | "top" | "bottom"
+		class: className,
+		...restProps
+	} = $props();
+</script>
+
+<div class="bits-checkbox" data-label-position={labelPosition} data-size={size}>
+	<Checkbox.Root {id} class={className} {...restProps} bind:checked />
+	{#if labelText}
+		<Label.Root for={id}>{labelText}</Label.Root>
+	{/if}
+</div>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+	}
+</style>
